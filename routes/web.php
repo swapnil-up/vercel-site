3e129ff4c1cff500f4b7299418a80178c9565cc8
swapnil-up <swapnilupadhyaykharel@gmail.com>
2025-03-05 22:12:29 +0545

    added vue and inertia

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Route::get('/ping', function () {
     return response()->json(['message' => 'pong']);
+});
+
+Route::get('/', function () {
+    return inertia('Index/Index');
 });
